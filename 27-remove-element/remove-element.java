d54f3f71da979da870b0f1d7f5cecae753d0fb89
SIVARAMAN G <99677088+rgsivaraman@users.noreply.github.com>
2023-08-23 23:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (37.00%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,17 +1,11 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
-        int reader=0,writer=0;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(nums[reader]==val){
-                reader++;
-            }
-            else{
-                nums[writer]=nums[reader];
-                writer++;
-                reader++;
-            }
-        }
-        return writer;
-    }
+public int removeElement(int[] nums, int val) {
+int writer = 0;
+int n=nums.length;
+for (int reader = 0; reader < n; reader++)
+ { if (nums[reader] != val) { nums[writer++] = nums[reader];
+}
+}
+return writer;
+}
 }
