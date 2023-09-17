9bb148d192d26d71b5370ba7603139ba20d8f62f
SIVARAMAN G <99677088+rgsivaraman@users.noreply.github.com>
2023-09-17 12:09:01 +0530

    Time: 3 ms (67.89%) | Memory: 47.2 MB (7.95%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int cnt1=0;int cnt2=0;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i]){
+                cnt1++;
+            }
+            else if(el2==nums[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        //manual check
+        List<Integer>ans=new ArrayList<>();
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==el1){
+                cnt1++;
+            }
+            if(nums[i]==el2){
+                cnt2++;
+            }
+        }
+        if(cnt1>nums.length/3) ans.add(el1);
+        if(cnt2>nums.length/3) ans.add(el2);
+        return ans;
+
+    }
+}
