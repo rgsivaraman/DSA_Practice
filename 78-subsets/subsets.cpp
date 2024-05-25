4b5c24ace71c4b88761170eb2236cde2e447e846
SIVARAMAN G <99677088+rgsivaraman@users.noreply.github.com>
2024-05-25 10:56:27 +0530

    Time: 3 ms (45.16%) | Memory: 8.3 MB (78.04%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    void backtrack(vector<int>& nums,int start,vector<int>& path,vector<vector<int>>& result){
-        result.push_back(path);
-        for(int i=start;i<nums.size();i++){
-            path.push_back(nums[i]);
-            backtrack(nums,i+1,path,result);
-            path.pop_back();
+    void backtrack(vector<int>& nums,int i,vector<int>& path,vector<vector<int>>& result){
+        if(i>=nums.size()){
+            result.push_back(path);
+            return;
         }
+        path.push_back(nums[i]);
+        backtrack(nums,i+1,path,result);
+        path.pop_back();
+        backtrack(nums,i+1,path,result);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> result;
