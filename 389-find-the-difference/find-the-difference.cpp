1dddc7453214531cafa0d476f42afaa0d0d013b7
SIVARAMAN G <99677088+rgsivaraman@users.noreply.github.com>
2024-09-26 14:58:18 +0530

    Time: 2 ms (67.78%) | Memory: 8.2 MB (56.73%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> ans(26,0);
+        for(int i=0;i<s.size();i++){
+            ans[s[i]-'a']++;
+        }
+        for(int i=0;i<t.size();i++){
+            ans[t[i]-'a']--;
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]!=0) return i+'a';
+        }
+        return ' ';
+    }
+};
